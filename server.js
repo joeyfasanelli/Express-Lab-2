e74f1816223420a697d081d56868df84b9fc921c
Joey Fasanelli <joeyfas1704@gmail.com>
2022-08-29 16:13:43 -0400

    Greetings express application created.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+app.get("/greeting", function (req, res) {
     res.send("Hello, stranger")
+});
+
+app.get("/greeting/:name", function (req, res) {
+    res.send("Hello, " + req.params.name)
 });
 
 app.listen(3000);
