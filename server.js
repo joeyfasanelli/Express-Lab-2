e2965b1dfeabf0cb403b71514765f683c156ade2
Joey Fasanelli <joeyfas1704@gmail.com>
2022-08-29 17:28:54 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -9,4 +9,11 @@ app.get("/greeting/:name", function (req, res) {
     res.send("Hello, " + req.params.name)
 });
 
+
+app.get("/tip/:total/:tipPercentage", function (req, res) {
+    res.send("Your tip is: $" + req.params.total * (req.params.tipPercentage / 100))
+});
+
+
+
 app.listen(3000);
